1062b13cc590055134b1f8ecb23252938b946afc
Boris Gerretzen <borisgerretzen@gmail.com>
2023-11-16 21:57:23 +0100

    move stuff around

--- GdsSharp.Lib/Parsing/Tokens/GdsHeader.cs
+++ GdsSharp.Lib/Parsing/Tokens/GdsHeader.cs
@@ -8,7 +8,7 @@ public class GdsHeader : IGdsSimpleRead, IGdsSimpleWrite
     ///     Total length of the record, including the header itself.
     ///     When setting make sure you include <see cref="RecordSize" />.
     /// </summary>
-    public ushort Length { get; set; }
+    public ushort Length { get; init; }
 
     /// <summary>
     ///     Number of bytes to read after the header.
@@ -18,7 +18,7 @@ public class GdsHeader : IGdsSimpleRead, IGdsSimpleWrite
     /// <summary>
     ///     Record type.
     /// </summary>
-    public ushort Code { get; set; }
+    public ushort Code { get; init; }
 
     public ushort GetLength()
     {
